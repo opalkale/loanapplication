44d2a60d4c974272ca5d0da96a58efe648ea6b1e
Opal Kale <opal.kale@gmail.com>
2015-02-25 12:57:11 -0800

    Creates a new folder via box API

--- server.rb
+++ server.rb
@@ -12,14 +12,8 @@ post '/' do
   HTTParty.post("https://api.box.com/2.0/folders/", 
     {
       :headers => { 'Authorization' => 'Bearer 9ap3MFMeWJcw2RE0X7cwSdWxJSpFk81P' },
-      :body => { "name" => @client_email, "parent" => {"id" => "0"} }
+      :body => { "name" => @client_email, "parent" => {"id" => "0"} }.to_json
     })
-  
-=begin
-  query   = { "name" => @client_email, "parent" => {"id" => "0"} }
-  headers = { "Authorization"=> "Bearer 9ap3MFMeWJcw2RE0X7cwSdWxJSpFk81P" }
-  HTTParty.post("https://api.box.com/2.0/folders/", :query => query, :headers => headers)
-=end
   
   redirect '/collaborate'
 end
